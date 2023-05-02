738876feb43d94dbe97b28d3f43852b86c85804e
leechangjoon <2018110021@dongguk.edu>
2023-05-03 00:57:44 +0900

    second commit

--- main/models.py
+++ main/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+# 이놈이 본체다. 얘 바뀔 때마다 migration (명령 2개)해줘야 함.
+
+class Blog(models.Model):
+    title = models.CharField(max_length=200)
+    writer = models.CharField(max_length=100)
+    pub_date = models.DateTimeField()
+    body = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        return self.body[:20]
+
